77e08a99d2c32f7c3fa620ca5cd2d9c9e0b9ae27
Tusar Kanta Marei <tusarkantamarei@gmail.com>
2026-08-28 18:17:31 +0530

    Update API_BASE URL to remove '/api' suffix

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const colors = {
     purple: '#8b5cf6'
 };
 
-const API_BASE = 'https://cave-evident-machines-contemporary.trycloudflare.com/api';
+const API_BASE = 'https://cave-evident-machines-contemporary.trycloudflare.com';
 
 async function init() {
     updateTime();
